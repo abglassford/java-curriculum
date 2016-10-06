67d670228673a315b7be3b20d5e61f285ed062c7
Alexander Glassford <a.b.glassford@gmail.com>
2016-10-06 10:59:13 -0600

    complete exercises 01 and 02

--- Unit-1-Syntax/Ops.java
+++ Unit-1-Syntax/Ops.java
@@ -6,9 +6,18 @@ public class Ops {
   public static int multiply(int x, int y) {
     return x * y;
   }
+  public static int power (int x) {
+    int y = 2;
+    int p = x;
+    for(int i = 0; i < y; i++) {
+      p = p * x;
+    }
+    return p;
+  }
 
   public static void main(String [] args) {
     System.out.println( sum(1, 2) );
     System.out.println( multiply(1, 2) );
+    System.out.println( power(3) );
   }
 }
